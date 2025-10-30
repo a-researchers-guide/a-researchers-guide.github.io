e23b6494a02be72e32720fba676394b4ea211c8a
dulranga <dulranga.webdesign@gmail.com>
2025-10-30 23:44:17 +0530

    added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
+import HeroSection from "./components/HeroSection";
+
 function App() {
   return (
-    <>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore hic
-      illum aperiam cupiditate delectus perspiciatis ullam molestias, et
-      temporibus nemo quidem in eius, nobis adipisci quisquam, repellat impedit
-      laborum voluptates.
-    </>
+    <main>
+      <HeroSection />
+    </main>
   );
 }
 
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,16 @@
+import type { FC } from "react";
+
+type HeroSectionProps = object;
+
+const HeroSection: FC<HeroSectionProps> = () => {
+  return (
+    <section className="font-medium text-4xl">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut rem commodi
+      laborum, fugiat sed dicta dolorem. Nulla optio officia nesciunt saepe
+      molestias dicta voluptates voluptatibus repellat consequuntur. Numquam, ab
+      inventore?
+    </section>
+  );
+};
+
+export default HeroSection;
