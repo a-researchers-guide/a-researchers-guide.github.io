4b0df5e9ad2c6cb64160b1e80d7bf07c2fa37180
dulranga <dulranga.webdesign@gmail.com>
2025-11-04 09:34:41 +0530

    cta section mobile responsive

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,13 +1,9 @@
 import NumberFlow from "@number-flow/react";
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, XIcon } from "lucide-react";
 import { useEffect, type FC } from "react";
 import Countdown, { type CountdownRendererFn } from "react-countdown";
 import { Button } from "./ui/button";
 import { EVENT_REGISTRATION_END } from "@/consts/event";
-
-// const registrationEndDate = new Date(
-//   "Fri Oct 20 2025 10:59:59 GMT+0530 (India Standard Time)"
-// );
 
 const CTA: FC = () => {
   useEffect(() => {
@@ -27,7 +23,7 @@ const CTA: FC = () => {
 
   const isRegClosed = new Date() > EVENT_REGISTRATION_END;
   return (
-    <div className="relative min-h-[80vh] grid place-items-center">
+    <div className="relative md:min-h-[80vh] grid place-items-center px-4">
       <div className="absolute inset-0 -z-10 opacity-12 masked--vertical">
         <div className="absolute size-full" id="CTASection"></div>
       </div>
@@ -37,9 +33,9 @@ const CTA: FC = () => {
             Register <span className="text-primary">Now</span>
           </h2>
         </div>
-        <div className="relative mx-auto w-fit my-20">
+        <div className="relative mx-auto w-fit md:my-20 my-10">
           {!isRegClosed && (
-            <div className="absolute top-0 tracking-wider uppercase">
+            <div className="absolute top-0 tracking-wider uppercase text-xs md:text-base">
               Time <span className="text-primary">Remaining</span>
             </div>
           )}
@@ -47,13 +43,20 @@ const CTA: FC = () => {
         </div>
 
         <div className="mx-auto block w-fit">
-          <Button disabled={isRegClosed} size={"lg"} className="pl-12! ">
+          <Button
+            disabled={isRegClosed}
+            size={"lg"}
+            className="md:pl-12! pl-6! pr-2!"
+          >
             {isRegClosed ? (
-              <>Registration Closed</>
+              <>
+                <span className="mr-2 md:mr-4">Registration Closed</span>
+                <XIcon className="md:size-8  p-2 md:p-4 rounded-full" />
+              </>
             ) : (
               <>
-                <span className="mr-4">Register Now</span>
-                <ChevronRightIcon className="size-8 bg-black text-primary p-4 rounded-full" />
+                <span className="mr-2 md:mr-4">Register Now</span>
+                <ChevronRightIcon className="md:size-8 bg-black text-primary  p-2 md:p-4 rounded-full" />
               </>
             )}
           </Button>
@@ -65,7 +68,9 @@ const CTA: FC = () => {
 
 // Random component
 const Completionist = () => (
-  <span className="text-7xl opacity-30">Sorry, Registration is Done.</span>
+  <span className="text-3xl md:text-5xl lg:text-7xl opacity-30 text-center">
+    Sorry, Registration is Done.
+  </span>
 );
 
 // Renderer callback with condition
@@ -82,7 +87,7 @@ const renderer: CountdownRendererFn = ({
   } else {
     // Render a countdown
     return (
-      <div className="flex gap-4">
+      <div className="flex items-center md:gap-4 py-2">
         <NumberBox number={days} label="Days" />
         <Dots />
         <NumberBox number={hours} label="Hours" />
@@ -100,22 +105,22 @@ const NumberBox: FC<{ number: number; label: string }> = ({
   label,
 }) => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="tabular-nums text-9xl font-medium">
+    <div className="flex flex-col items-center relative">
+      <div className="tabular-nums md:text-8xl lg:text-9xl text-5xl font-medium">
         <NumberFlow
           trend={-1}
           value={number}
           format={{ minimumIntegerDigits: 2 }}
         />
       </div>
-      <div className="mt-2 text-md text-foreground/60 tracking-wider">
+      <div className="md:mt-2 text-xs md:text-base text-md text-foreground/60 tracking-wider absolute top-full text-center w-full">
         {label}
       </div>
     </div>
   );
 };
 const Dots: FC = () => {
-  return <span className="text-9xl">:</span>;
+  return <span className="md:text-8xl lg:text-9xl text-5xl ">:</span>;
 };
 
 export default CTA;
