7736336bd49663b868e437d7a11c1e119be36905
dulranga <dulranga.webdesign@gmail.com>
2025-11-26 18:09:47 +0530

    changed attendee count

--- src/components/ExpectedAttendence.tsx
+++ src/components/ExpectedAttendence.tsx
@@ -5,9 +5,9 @@ const ExpectedAttendence: FC = () => {
   return (
     <Card className="max-w-md w-full border-none bg-transparent lg:bg-card/40 h-auto">
       <CardContent className="grid place-content-center place-items-center h-fit my-auto">
-        <div className="text-8xl md:text-[140px] font-extrabold mb-5 whitespace-nowrap ">
+        <div className="text-8xl md:text-[100px] font-extrabold mb-5 whitespace-nowrap ">
           <span className="opacity-0 select-none pointer-events-none">+</span>
-          <span className="text-primary">75</span>+
+          <span className="text-primary">100</span>+
         </div>
         <p className="md:text-xl">Delegates</p>
       </CardContent>
