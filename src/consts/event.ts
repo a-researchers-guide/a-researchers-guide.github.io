2de1686fba76cc119905456a10eda9a51e8bbcbd
dulranga <dulranga.webdesign@gmail.com>
2025-12-05 16:59:28 +0530

    changed deadline

--- src/consts/event.ts
+++ src/consts/event.ts
@@ -5,7 +5,7 @@ export const EVENT_END = new Date(
   "Sat Dec 13 2025 14:00:00 GMT+0530 (India Standard Time)"
 );
 export const EVENT_REGISTRATION_END = new Date(
-  "Sun Dec 07 2025 23:59:59 GMT+0530 (India Standard Time)"
+  "Tue Dec 09 2025 23:59:59 GMT+0530 (India Standard Time)"
 );
 
 export const REGISTRATION_LINK = "https://forms.gle/TcSYJyxT4hkvdELUA";
